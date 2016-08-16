39558b07d778daf054756145a4471f53ef261cc8
Mikael Karon <mikael@karon.se>
2016-08-16 16:55:03 +0530

    Initial commit

---
+++ jquery.crank.js
@@ -0,0 +1,36 @@
+(function($, when, slice) {
+
+  function crank(attr) {
+    var self = this;
+    var args = slice.call(arguments, 1);
+
+    return when.apply(null, this.map(function(index, element) {
+      var $element = $(element);
+
+      return when.apply(null, $element
+        .attr(attr)
+        .split(/\s+/)
+        .map(function(module) {
+          return $element.triggerHandler([attr, module].join("."), args) || [$element, module];
+        }));
+    }));
+  }
+
+  $.fn.crank = function() {
+    var self = this;
+    var args = arguments;
+
+    return $.Deferred(function(deferred) {
+      try {
+        when(crank.apply(self, args)).then(
+          function() {
+            deferred.resolveWith(self, arguments);
+          }, function() {
+            deferred.rejectWith(self, arguments);
+          });
+      } catch (e) {
+        deferred.rejectWith(self, [e]);
+      }
+    }).promise();
+  };
+})(jQuery, jQuery.when, Array.prototype.slice);
