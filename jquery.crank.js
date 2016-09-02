64cd8e3350e696ef3363698c7d36e09d5befc52f
Mikael Karon <mikael@karon.se>
2016-09-02 23:05:22 +0200

    use $.when over when

--- jquery.crank.js
+++ jquery.crank.js
@@ -8,20 +8,19 @@
   }
 }(this, function($) {
   var slice = Array.prototype.slice;
-  var when = $.when;
 
   function crank(attr, eventType) {
     var self = this;
     var args = slice.call(arguments, 2);
 
-    return when.apply(null, this.map(function(index, element) {
+    return $.when.apply(null, this.map(function(index, element) {
       var $element = $(element);
 
-      return when.apply(null, $element
+      return $.when.apply(null, $element
         .attr(attr)
         .split(/\s+/)
         .map(function(module) {
-          return when($element.triggerHandler(eventType + "." + module, args)).then(function(result) {
+          return $.when($element.triggerHandler(eventType + "." + module, args)).then(function(result) {
             return arguments.length > 1 ? slice.call(arguments) : result || module;
           });
         }));
@@ -34,7 +33,7 @@
 
     return $.Deferred(function(deferred) {
       try {
-        when(crank.apply(self, args)).then(
+        $.when(crank.apply(self, args)).then(
           function() {
             deferred.resolveWith(self, arguments);
           }, function() {
