803e20417d9b3a7f9fe7f1685d40588674039090
Mikael Karon <mikael@karon.se>
2016-09-08 20:18:51 +0200

    fix: UMD fixes

--- jquery.crank.js
+++ jquery.crank.js
@@ -2,14 +2,12 @@
   if (typeof define === "function" && define.amd) {
     define(modules, factory);
   } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.apply(root, modules.map(function(m) {
-      return require(m);
-    }));
+    module.exports = factory.apply(root, modules.map(require));
   } else {
     root["mu-jquery-crank/jquery.crank"] = factory.apply(root, modules.map(function(m) {
       return {
-          "jquery": root.jQuery
-        }[m] || root[m];
+        "jquery": root.jQuery
+      }[m] || root[m];
     }));
   }
 })([
