48559503189be1d357c3c3e115e1712bdf88e972
Mikael Karon <mikael@karon.se>
2016-10-12 15:26:48 +0200

    reformat

--- jquery.crank.js
+++ jquery.crank.js
@@ -1,26 +1,26 @@
-(function(modules, root, factory) {
+(function (modules, root, factory) {
   if (typeof define === "function" && define.amd) {
     define(modules, factory);
   } else if (typeof module === "object" && module.exports) {
     module.exports = factory.apply(root, modules.map(require));
   } else {
-    root["mu-jquery-crank/jquery.crank"] = factory.apply(root, modules.map(function(m) {
-      return {
+    root["mu-jquery-crank/jquery.crank"] = factory.apply(root, modules.map(function (m) {
+      return this[m] || root[m];
+    }, {
         "jquery": root.jQuery
-      }[m] || root[m];
-    }));
+      }));
   }
 })([
   "jquery",
   "mu-jquery-wire/jquery.wire"
-], this, function($, wire) {
+], this, function ($, wire) {
   var slice = Array.prototype.slice;
 
-  return function(input, eventType) {
+  return function (input, eventType) {
     var args = slice.call(arguments, 2);
 
-    return wire.call(this, input, function($element, index, ns) {
-      return $.when($element.triggerHandler(eventType + "." + ns, args)).then(function(result) {
+    return wire.call(this, input, function ($element, index, ns) {
+      return $.when($element.triggerHandler(eventType + "." + ns, args)).then(function (result) {
         return arguments.length > 1 ? slice.call(arguments) : result || ns;
       });
     });
