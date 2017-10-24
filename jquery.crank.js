2cca0c9dad5b7b4878fd202d386412f32ec06e1b
Mikael Karon <mikael@karon.se>
2017-10-24 23:46:57 +0200

    fix: use element jquery vs global jquery (#3)

--- jquery.crank.js
+++ jquery.crank.js
@@ -13,7 +13,8 @@
     var args = slice.call(arguments, 2);
 
     return wire.call(this, input, function ($element, index, ns) {
-      return $element.constructor.when.apply(null, $.makeArray($element.triggerHandler(eventType + "." + ns, args))).then(function (result) {
+      var $ = $element.constructor;
+      return $.when.apply(null, $.makeArray($element.triggerHandler(eventType + "." + ns, args))).then(function (result) {
         return arguments.length > 1 ? slice.call(arguments) : result || ns;
       });
     });
